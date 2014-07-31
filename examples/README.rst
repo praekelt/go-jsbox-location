ed81f3868a5f53735577935800db62b2c67c410c
Rudi Giesler <rudi@praekelt.com>
2014-07-31 13:52:31 +0200

    Added application example

---
+++ examples/README.rst
@@ -0,0 +1,5 @@
+A location app
+==============
+
+Shows the basics for using the LocationState state to store location data to the
+contact store, and how to test location-based apps.
---
+++ examples/lib/app.js
@@ -0,0 +1,54 @@
+var vumigo = require('vumigo_v02');
+var LocationState = require('../../lib');
+
+var App = vumigo.App;
+var EndState = vumigo.states.EndState;
+
+var LocationApp = App.extend(function(self){
+    App.call(self, 'states:start');
+
+    self.states.add('states:start', function(name){
+        // This state will get the location data from the user and store it in 
+        // the contact store. We will store and return the longitude and 
+        // latitude information to the user.
+        return new LocationState(name, {
+            question: ["Welcome to the location app.", 
+                "What is your current address?"].join("\n"),
+            next: "states:end",
+            previous_text: "Prev",
+            store_fields: ["geometry.location", "formatted_address"]
+        });
+    });
+
+    self.states.add('states:end', function(name) {
+        // This state gets the stored location data for the user, and returns
+        // the information back to the user. This state ends the application.
+
+        // First we get the contact information
+        return self.im
+            .contacts.for_user()
+            .then(function(user_contact) {
+                self.contact = user_contact;
+                // Then we return that information to the user
+                var address = self.contact.extra['location:formatted_address'];
+                var longitude = self.contact.extra
+                    ['location:geometry:location:longitude'];
+                var latitude = self.contact.extra
+                    ['location:geometry:location:latitude'];
+                return new EndState(name, {
+                    text: [
+                        'The location',
+                        '"' + address + '"',
+                        'is located at longitude',
+                        longitude,
+                        'and latitude',
+                        latitude
+                        ].join(' ')
+                });
+            });
+    });
+
+});
+
+vumigo.interact(this.api, LocationApp);
+this.LocationApp = LocationApp;
