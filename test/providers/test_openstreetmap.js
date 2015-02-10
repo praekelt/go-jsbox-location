7eb85db7bcbf2572ba06d439fbcef710cff5e96f
Simon Cross <hodgestar@gmail.com>
2015-02-10 15:36:51 +0200

    Add OpenStreetMap.fixture.

--- test/providers/test_openstreetmap.js
+++ test/providers/test_openstreetmap.js
@@ -276,3 +276,78 @@ describe('OpenStreetMap', function() {
         });
     });
 });
+
+describe.only('OpenStreetMap.fixture', function() {
+    it('should return a fixture', function() {
+        assert.deepEqual(
+            OpenStreetMap.fixture({
+                query: "Baker Street",
+                address_list: ["2B", "Not 2B"],
+            }),
+            {
+               request: {
+                   method: "GET",
+                   url: "http://open.mapquestapi.com/nominatim/v1/search.php",
+                   params: {
+                       format: "json",
+                       q: "Baker Street",
+                       addressdetails: "1",
+                       bounded: "1",
+                       limit: "30",
+                       viewbox: "viewbox=-180.0%2C90.0%2C180.0%2C-90.0"
+                   },
+               },
+               response: {
+                   code: 200,
+                   data: [
+                       {
+                           display_name: "2B",
+                       },
+                       {
+                           display_name: "Not 2B",
+                       },
+                   ],
+               },
+            }
+        );
+    });
+
+    it('should require a query parameter', function() {
+        assert.throws(
+            function() {
+                OpenStreetMap.fixture({
+                    address_list: ["2B"],
+                });
+            },
+            providers.FixtureParameterMissingError
+        );
+    });
+
+    it('should allow overriding the request url', function() {
+       var fixture = OpenStreetMap.fixture({
+           query: "Where am I?",
+           request_url: "http://www.example.com",
+       });
+       assert.strictEqual(fixture.request.url, "http://www.example.com");
+    });
+
+    it('should default to an empty address list', function() {
+       var fixture = OpenStreetMap.fixture({
+           query: "Place with no places",
+       });
+       assert.deepEqual(fixture.response.data, []);
+    });
+
+    it('should allow overriding the response data', function() {
+        var fixture = OpenStreetMap.fixture({
+            query: "Can has custom data?",
+            address_list: ["Ignored place"],
+            response_data: [
+                {place: 1}, {place: 2},
+            ],
+        });
+        assert.deepEqual(fixture.response.data, [
+            {place: 1}, {place: 2},
+        ]);
+    });
+});
